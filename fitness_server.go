b89c8e098e682d4c1ce6c5e2d866bfed9abc11a9
Cdl123890 <sendrosjeffaith@gmail.com>
2025-08-08 05:31:05 -0700

    Create fitness_server.go

---
+++ fitness_server.go
@@ -0,0 +1,24 @@
+// fitness_server.go
+package main
+
+import (
+    "encoding/json"
+    "math/rand"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    tips := []string{
+        "Do 15-minute bodyweight HIIT.",
+        "Walk 10,000 steps a day.",
+        "Stretch every morning for 5 minutes.",
+    }
+    json.NewEncoder(w).Encode(map[string]string{
+        "tip": tips[rand.Intn(len(tips))],
+    })
+}
+
+func main() {
+    http.HandleFunc("/api/fitness", handler)
+    http.ListenAndServe(":5003", nil)
+}
